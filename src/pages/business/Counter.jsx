4c27a92c56530342b68642e72dc5030790a98671
Mehul Khandelwal <mehulkhandelwal114@gmail.com>
2026-09-19 14:18:32 +0530

    Fix missing Plus icon import in Counter.jsx causing Party picker crash

--- src/pages/business/Counter.jsx
+++ src/pages/business/Counter.jsx
@@ -1,5 +1,5 @@
 import { useState, useMemo } from "react";
-import { ShoppingCart, Check } from "lucide-react";
+import { ShoppingCart, Check, Plus } from "lucide-react";
 import Card from "../../components/Card";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
